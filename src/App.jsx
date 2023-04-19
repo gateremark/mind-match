de9744799d7ee2ef295663edde1eb31a9946eed5
gateremark <mark5gatere@gmail.com>
2023-04-20 01:31:07 +0300

    Added a css grid for the images

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
 import { useState } from 'react'
 import './App.css'
 
+const myCards = [
+  { "src": "/img/go.png"},
+  { "src": "/img/js.png"},
+  { "src": "/img/php.png"},
+  { "src": "/img/python.png"},
+  { "src": "/img/react.png"},
+  { "src": "/img/rust.png"}
+]
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [cards, setCards] = useState([])
+  const [flipped, setFlipped] = useState(0)
+
+  // shuffle cards
+  const shuffle = () => {
+    const shuffled = [...myCards, ...myCards]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({...card, id: Math.random()}))
+  
+    setCards(shuffled)
+    setFlipped(0)
+  }
+
+  console.log(cards, flipped)
 
   return (
     <div className="App">
       <h1>Mind Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffle}>New Game</button>
+      <div className='card___grid'>
+        {cards.map((card) => (
+          <div className='card' key={card.id}>
+            <div>
+                <img className='front' src={card.src} alt="Card Front" />
+                <img className='back' src="/img/cover.png" alt="Card Back" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
